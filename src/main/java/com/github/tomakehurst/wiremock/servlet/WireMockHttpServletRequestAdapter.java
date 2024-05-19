1a2eaada3a6f9a1b7fd56d1eeb48bb8281d550d1
mufasa1976 <mufasa1976@coolstuff.software>
2024-05-19 11:22:32 +0200

    backward pull from master branch of multipart/related issue

--- src/main/java/com/github/tomakehurst/wiremock/servlet/WireMockHttpServletRequestAdapter.java
+++ src/main/java/com/github/tomakehurst/wiremock/servlet/WireMockHttpServletRequestAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2022 Thomas Akehurst
+ * Copyright (C) 2016-2024 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -292,7 +292,7 @@ public class WireMockHttpServletRequestAdapter implements Request {
   @Override
   public boolean isMultipart() {
     String header = getHeader("Content-Type");
-    return (header != null && header.contains("multipart/"));
+    return (header != null && header.matches("(?i)^\\s*multipart/.*"));
   }
 
   @Override
